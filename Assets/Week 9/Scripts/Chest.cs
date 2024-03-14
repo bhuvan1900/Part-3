7fad33a27059546f51bb9cc2ffc1fde2dfd25063
Bhuvan Chalasani <chalasbh@shernet.sheridancollege.ca>
2024-03-14 12:10:15 -0400

    task 4a incomplete

--- Assets/Week 9/Scripts/Chest.cs
+++ Assets/Week 9/Scripts/Chest.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public enum ChestType { Villager, Merchant, Archer }
+public enum ChestType { Villager, Merchant, Archer, Thief }
 
 public class Chest : MonoBehaviour
 {
