5223d16b3572726c1dad5b4baa790e68abf8acd1
Bhuvan Chalasani <chalasbh@shernet.sheridancollege.ca>
2024-03-21 11:01:15 -0400

    Week10-task2

---
+++ Assets/Week 10/Scripts/Clock.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour
+{
+
+    public Slider slider;
+    float time;
+    public float speed;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime * speed;
+        time = time % 60;
+        slider.value = time;
+    }
+}
