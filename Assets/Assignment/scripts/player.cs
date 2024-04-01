ed27c206f398ea962d6efca066bd358e51aaa519
Bhuvan Chalasani <chalasbh@shernet.sheridancollege.ca>
2024-03-31 22:44:14 -0400

    setting up base

--- Assets/Assignment/scripts/player.cs
+++ Assets/Assignment/scripts/player.cs
@@ -6,13 +6,10 @@ public class player : MonoBehaviour
 {
     public float playerSpeed;
     
-
-    
     private void Update()
     {
         float direction = Input.GetAxis("Horizontal");
         transform.Translate (direction * playerSpeed * Time.deltaTime,0, 0);
-
 
     }
 }
